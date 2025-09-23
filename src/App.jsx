4d0751cb01aee30a74b43238ad39a10206a57bcf
looks32 <wheodnjs2@naver.com>
2025-09-23 15:00:45 +0900

    Button, Header 컴포넌트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import New from './pages/New';
-import Diary from './pages/Diary';
-import Notfound from './pages/Notfound';
-import { getEmotionImage } from './utill/get-emotion-image';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./utill/get-emotion-image";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   const nav = useNavigate();
 
   const onClickButton = () => {
-    nav('/new');
+    nav("/new");
   };
 
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} alt="" />
-        <img src={getEmotionImage(2)} alt="" />
-        <img src={getEmotionImage(3)} alt="" />
-        <img src={getEmotionImage(4)} alt="" />
-        <img src={getEmotionImage(5)} alt="" />
-      </div>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>new</Link>
-        <Link to={'/diary'}>diary</Link>
-        <button onClick={onClickButton}>new 페이지로 이동</button>
-      </div>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"left"} />}
+        rightChild={<Button text={"right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123 클릭");
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
