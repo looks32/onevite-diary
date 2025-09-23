eb02d894cbdfa826b510147344128270f72ba039
looks32 <wheodnjs2@naver.com>
2025-09-23 15:19:22 +0900

    useReducer 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,38 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-import { getEmotionImage } from "./utill/get-emotion-image";
-import Button from "./components/Button";
-import Header from "./components/Header";
+import Edit from "./pages/Edit";
+import { useReducer } from "react";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"left"} />}
-        rightChild={<Button text={"right"} />}
-      />
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("123 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123 클릭");
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
         <Route path="/diary/:id" element={<Diary />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
         <Route path="*" element={<Notfound />}></Route>
       </Routes>
     </>
