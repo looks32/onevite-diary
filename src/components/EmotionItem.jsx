da73204927228dcf77b86b9341670ae1a79d961c
looks32 <wheodnjs2@naver.com>
2025-10-06 23:26:38 +0900

    New 페이지 UI 구성

---
+++ src/components/EmotionItem.jsx
@@ -0,0 +1,17 @@
+import { getEmotionImage } from '../utill/get-emotion-image';
+import './EmotionItem.css';
+
+const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+  return (
+    <div
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on_${emotionId}` : ''
+      }`}
+    >
+      <img className="emotion_img" src={getEmotionImage(emotionId)} alt="" />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
